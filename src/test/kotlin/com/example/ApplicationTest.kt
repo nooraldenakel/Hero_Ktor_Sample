a3321202167387d9aef5369ae432197b94cd720d
muslim <n00rALDENakel>
2021-12-29 14:28:21 +0300

    add new files.

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,15 +1,10 @@
 package com.example
 
-import io.ktor.routing.*
+import com.example.plugins.configureRouting
 import io.ktor.http.*
-import io.ktor.features.*
-import io.ktor.serialization.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import com.example.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
